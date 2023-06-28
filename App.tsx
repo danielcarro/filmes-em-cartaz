f8a413c3ac788164f8e34942fe70c4bebe8f9d4d
danielcarro <67878930+danielcarro@users.noreply.github.com>
2023-06-28 19:49:28 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, Button, FlatList, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 
-export default function App() {
+type Movie = {
+  titulo: string;
+  avatar: string;
+}
+const App = () => {
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const requjestMovies = async () => {
+      setLoading(true);
+      const req = await fetch('https://api.b7web.com.br/cinema/');
+      const json = await req.json();
+      if (json) {
+        setMovies(json);
+        setLoading(false);
+      }
+    }
+    requjestMovies();
+  }, []);
+
+  const handleLoadButton = async () => {
+    setLoading(true);
+    const req = await fetch('https://api.b7web.com.br/cinema/');
+    const json = await req.json();
+    if (json) {
+      setMovies(json);
+      setLoading(false);
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title='carregar Filmes' onPress={handleLoadButton} />
+      {loading &&
+        <View style={styles.loadingArea}>
+          <ActivityIndicator size='large' color='#fff' />
+          <Text style={styles.loadingText}>Carregando...</Text>
+        </View>
+      }
+
+
+
+      {!loading &&
+        <>
+          <Text style={styles.totalMoviesText}>Total de Filmes: {movies.length}</Text>
+          <FlatList
+            style={styles.list}
+            data={movies}
+            renderItem={({ item }) => (
+              <View style={styles.movieItem}>
+                <Image
+                  source={{ uri: item.avatar }}
+                  style={styles.movieImage}
+                  resizeMode='contain'
+                />
+                <Text style={styles.movieTitle}>{item.titulo}</Text>
+              </View>
+            )}
+            keyExtractor={item => item.titulo}
+          />
+        </>
+      }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#333'
   },
-});
+  totalMoviesText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  list: {
+    flex: 1
+  },
+  movieItem: {
+    marginBottom: 30
+  },
+  movieImage: {
+    height: 400
+  },
+  movieTitle: {
+    color: '#fff',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 5
+  },
+  loadingArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loadingText: {
+    color: '#fff',
+    textAlign: 'center'
+  }
+})
+
+export default App;
