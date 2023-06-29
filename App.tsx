8c635f7fde6eae7667447b1a442dbdbc3569f198
danielcarro <67878930+danielcarro@users.noreply.github.com>
2023-06-28 21:38:42 -0300

    second commit

--- App.tsx
+++ App.tsx
@@ -1,109 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, Button, FlatList, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, Button, StyleSheet, TextInput } from 'react-native';
 
-type Movie = {
-  titulo: string;
-  avatar: string;
-}
 const App = () => {
 
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState<Boolean>(false);
+    const [title, setTitle] = useState<string>();
+    const [body, setBody] = useState<string>();
 
-  useEffect(() => {
-    const requjestMovies = async () => {
-      setLoading(true);
-      const req = await fetch('https://api.b7web.com.br/cinema/');
-      const json = await req.json();
-      if (json) {
-        setMovies(json);
-        setLoading(false);
-      }
+    const handleSend = async () => {
+        if (title != '' && body != '') {
+            const req = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: title,
+                    body: body,
+                    userId: 326
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const json = await req.json();
+          
+                alert('Salvo com sucesso! '+ json.id);
+           
+        } else {
+            alert('Preencha as informações');
+        }
     }
-    requjestMovies();
-  }, []);
 
-  const handleLoadButton = async () => {
-    setLoading(true);
-    const req = await fetch('https://api.b7web.com.br/cinema/');
-    const json = await req.json();
-    if (json) {
-      setMovies(json);
-      setLoading(false);
-    }
-  }
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button title='carregar Filmes' onPress={handleLoadButton} />
-      {loading &&
-        <View style={styles.loadingArea}>
-          <ActivityIndicator size='large' color='#fff' />
-          <Text style={styles.loadingText}>Carregando...</Text>
-        </View>
-      }
-
-
-
-      {!loading &&
-        <>
-          <Text style={styles.totalMoviesText}>Total de Filmes: {movies.length}</Text>
-          <FlatList
-            style={styles.list}
-            data={movies}
-            renderItem={({ item }) => (
-              <View style={styles.movieItem}>
-                <Image
-                  source={{ uri: item.avatar }}
-                  style={styles.movieImage}
-                  resizeMode='contain'
-                />
-                <Text style={styles.movieTitle}>{item.titulo}</Text>
-              </View>
-            )}
-            keyExtractor={item => item.titulo}
-          />
-        </>
-      }
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.label}>Titulo:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={t => setTitle(t)} />
+            <Text style={styles.label}>Body:</Text>
+            <TextInput style={styles.input} value={body} onChangeText={t => setBody(t)} />
+            <Button  title='Enviar' onPress={handleSend} />
+        </SafeAreaView>
+    );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333'
-  },
-  totalMoviesText: {
-    color: '#fff',
-    fontSize: 18,
-    textAlign: 'center',
-    marginTop: 10,
-    marginBottom: 10
-  },
-  list: {
-    flex: 1
-  },
-  movieItem: {
-    marginBottom: 30
-  },
-  movieImage: {
-    height: 400
-  },
-  movieTitle: {
-    color: '#fff',
-    fontSize: 24,
-    textAlign: 'center',
-    marginTop: 5
-  },
-  loadingArea: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  loadingText: {
-    color: '#fff',
-    textAlign: 'center'
-  }
-})
+    container: {
+        flex: 1,
+        backgroundColor: '#000000'
+    },
+    label: {
+        color: '#fff',
+        padding: 10
+    },
+    input: {       
+        padding: 10,
+        margin: 10,
+        backgroundColor: "#ccc",
+        color: '#000',
+        borderRadius: 5
+    }
+});
 
 export default App;
